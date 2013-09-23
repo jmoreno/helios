034d1e9c18394e547ea3474459894c6547774cdb
Javi Moreno <jmoreno.zn@gmail.com>
2013-09-23 18:16:36 +0200

    Creación de fichero para gestión de Configuraciones remotas

---
+++ lib/helios/backend/remote-configuration.rb
@@ -0,0 +1,16 @@
+require 'rack/remote-configuration'
+
+require 'sinatra/base'
+require 'sinatra/param'
+
+
+class Helios::Backend::RemoteConfiguration < Sinatra::Base
+  helpers Sinatra::Param
+
+  def initialize(app, options = {})
+    super(Rack::Scaffold.new(options))
+
+    @configuration = Rack::RemoteConfiguration.new(options[:configuration]) rescue nil
+  end
+
+end
