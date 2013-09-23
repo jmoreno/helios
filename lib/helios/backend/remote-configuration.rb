2e8630bd8f977510547b840c382c4abcabab016a
Javi Moreno <jmoreno.zn@gmail.com>
2013-09-23 18:17:11 +0200

    Update remote-configuration.rb

--- lib/helios/backend/remote-configuration.rb
+++ lib/helios/backend/remote-configuration.rb
@@ -8,7 +8,7 @@ class Helios::Backend::RemoteConfiguration < Sinatra::Base
   helpers Sinatra::Param
 
   def initialize(app, options = {})
-    super(Rack::Scaffold.new(options))
+    super(Rack::RemoteConfiguration.new(options))
 
     @configuration = Rack::RemoteConfiguration.new(options[:configuration]) rescue nil
   end
